6d752050d3d44716de87700730d425f0ebe7ae05
Runner <runner@example.com>
2023-10-07 13:33:14 +0300

    chore: reformat files

--- src/routes/(dashboard)/invoices/+page.svelte
+++ src/routes/(dashboard)/invoices/+page.svelte
@@ -11,14 +11,15 @@
 </svelte:head>
 
 <div
-	class="flex flex-col-reverse md:flex-row items-start justify-between md:items-center lg:mb-16 mb-7 gap-y-6 md:gap-y-4"
+	class='flex flex-col-reverse md:flex-row items-start justify-between md:items-center lg:mb-16 mb-7 gap-y-6 md:gap-y-4'
 >
 	<Search />
 	<!--  New Invoices -->
 	<div>
 		<button
-			class="relative whitespace-nowrap rounded-lg bg-lavenderIndigo px-5 lg:px-10 py-2 lg:py-3 font-sansSerif text-base lg:text-xl font-black text-white shadow-colored hover:shadow-coloredHover translate-y-0 hover:-translate-y-2 transition-all"
-			>+ Invoices</button
+			class='relative whitespace-nowrap rounded-lg bg-lavenderIndigo px-5 lg:px-10 py-2 lg:py-3 font-sansSerif text-base lg:text-xl font-black text-white shadow-colored hover:shadow-coloredHover translate-y-0 hover:-translate-y-2 transition-all'
+		>+ Invoices
+		</button
 		>
 	</div>
 </div>
@@ -26,10 +27,10 @@
 <!--list of invoices-->
 <div>
 	<!--  header  -->
-	<div class="table-header hidden lg:grid invoice-table text-daisyBush">
+	<div class='invoice-table table-header hidden lg:grid text-daisyBush'>
 		<h3>Status</h3>
 		<h3>Due date</h3>
-		<h3>Invoice ID</h3>
+		<h3>ID</h3>
 		<h3>Client</h3>
 		<h3>Amount</h3>
 		<div />
@@ -37,58 +38,73 @@
 	</div>
 	<!--  invoices -->
 	<div
-		class="invoice-table invoice-row items-center bg-white py-3 lg:py-6 rounded-lg shadow-tableRow"
+		class='invoice-table invoice-row items-center bg-white py-3 lg:py-6 rounded-lg shadow-tableRow'
 	>
-		<div class="status"><Tag className="ml-auto lg:ml-0" label="paid" /></div>
-		<div class="text-sm lg:text-lg dueDate">19.9.2023</div>
-		<div class="text-sm lg:text-lg invoiceNumber">12345</div>
-		<div class="text-base lg:text-xl font-bold clientName">Flamingo client</div>
-		<div class="text-sm lg:text-lg font-mono font-bold amount text-right">1000$</div>
-		<div class="text-sm lg:text-lg center viewButton hidden lg:block">
-			<a href="#" class="text-pastelPurple hover:text-daisyBush"><View /></a>
+		<div class='status'>
+			<Tag className='ml-auto lg:ml-0' label='paid' />
 		</div>
-		<div class="center text-lg moreButton hidden lg:block">
-			<button class="text-pastelPurple hover:text-daisyBush"><More /></button>
+		<div class='text-sm lg:text-lg dueDate'>19.9.2023</div>
+		<div class='text-sm lg:text-lg invoiceNumber'>12345</div>
+		<div class='text-base lg:text-xl font-bold clientName'>Flamingo client</div>
+		<div class='text-sm lg:text-lg font-mono font-bold amount'>1000$</div>
+		<div class='text-sm lg:text-lg center viewButton hidden lg:block'>
+			<a href='#' class='text-pastelPurple hover:text-daisyBush'>
+				<View />
+			</a>
+		</div>
+		<div class='center text-lg moreButton hidden lg:block'>
+			<button class='text-pastelPurple hover:text-daisyBush'>
+				<More />
+			</button>
 		</div>
 	</div>
 </div>
 
-<CircleAmount label="Total" amount="1000$" />
+<CircleAmount label='Total' amount='1000$' />
 
-<style lang="postcss">
-	.table-header h3 {
-		@apply text-xl font-black leading-snug;
-	}
-	.invoice-row {
-		grid-template-areas:
+<style lang='postcss'>
+    .table-header h3 {
+        @apply text-xl font-black leading-snug;
+    }
+
+    .invoice-row {
+        grid-template-areas:
 			'invoiceNumber invoiceNumber'
 			'clientName amount'
 			'status dueDate';
-	}
-	@media (min-width: 1024px) {
-		.invoice-row {
-			grid-template-areas: 'status dueDate invoiceNumber clientName amount viewButton moreButton';
-		}
-	}
-	.invoice-row .status {
-		grid-area: status;
-	}
-	.invoice-row .dueDate {
-		grid-area: dueDate;
-	}
-	.invoice-row .clientName {
-		grid-area: clientName;
-	}
-	.invoice-row .amount {
-		grid-area: amount;
-	}
-	.invoice-row .viewButton {
-		grid-area: viewButton;
-	}
-	.invoice-row .moreButton {
-		grid-area: moreButton;
-	}
-	.invoice-row .invoiceNumber {
-		grid-area: invoiceNumber;
-	}
+    }
+
+    @media (min-width: 1024px) {
+        .invoice-row {
+            grid-template-areas: 'status dueDate invoiceNumber clientName amount viewButton moreButton';
+        }
+    }
+
+    .invoice-row .status {
+        grid-area: status;
+    }
+
+    .invoice-row .dueDate {
+        grid-area: dueDate;
+    }
+
+    .invoice-row .clientName {
+        grid-area: clientName;
+    }
+
+    .invoice-row .amount {
+        grid-area: amount;
+    }
+
+    .invoice-row .viewButton {
+        grid-area: viewButton;
+    }
+
+    .invoice-row .moreButton {
+        grid-area: moreButton;
+    }
+
+    .invoice-row .invoiceNumber {
+        grid-area: invoiceNumber;
+    }
 </style>
