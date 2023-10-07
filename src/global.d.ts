daadb9ba13a95dc59ae85f969417ec9962cae4a7
Runner <runner@example.com>
2023-10-07 14:32:00 +0300

    feat:add type definitions for invoice

---
+++ src/global.d.ts
@@ -0,0 +1,32 @@
+import { ClientStatus, InvoiceStatus } from './enums';
+
+interface Invoice {
+	client: Client;
+	createdAt: string;
+	dueDate: string;
+	id: string;
+	invoiceNumber: string;
+	invoiceStatus: InvoiceStatus;
+	issueDate: string;
+	lineItems?: LineItem[];
+	notes?: string;
+	subject?: string;
+	terms?: string;
+}
+
+interface Client {
+	clientStatus: ClientStatus;
+	city: string;
+	email: string;
+	id: string;
+	state: string;
+	street: string;
+	zip: string;
+}
+
+interface LineItem {
+	amount: number;
+	description: string;
+	id: string;
+	quantity: number;
+}
--- src/lib/components/Tag.svelte
+++ src/lib/components/Tag.svelte
@@ -1,5 +1,5 @@
-<script lang="ts">
-	export let label: 'draft' | 'sent' | 'late' | 'paid' = 'draft';
+<script lang='ts'>
+	export let label: 'draft' | 'sent' | 'late' | 'paid' | 'archive' = 'draft';
 	export let className = '';
 </script>
 
@@ -8,22 +8,25 @@
 	class:inverted={label === 'draft'}
 	class:success={label === 'sent' || label === 'current'}
 	class:alert={label === 'late'}
-	class:archive={label === 'paid'}
+	class:archive={label === 'paid' || label === 'archive'}
 >
 	{label}
 </div>
 
-<style lang="postcss">
-	.inverted {
-		@apply border-pastelPurple text-pastelPurple;
-	}
-	.success {
-		@apply border-robinEggBlue bg-robinEggBlue text-bluegem;
-	}
-	.alert {
-		@apply border-scarlet bg-scarlet text-goldenFizz;
-	}
-	.archive {
-		@apply border-pastelPurple bg-pastelPurple text-white;
-	}
+<style lang='postcss'>
+    .inverted {
+        @apply border-pastelPurple text-pastelPurple;
+    }
+
+    .success {
+        @apply border-robinEggBlue bg-robinEggBlue text-bluegem;
+    }
+
+    .alert {
+        @apply border-scarlet bg-scarlet text-goldenFizz;
+    }
+
+    .archive {
+        @apply border-pastelPurple bg-pastelPurple text-white;
+    }
 </style>
